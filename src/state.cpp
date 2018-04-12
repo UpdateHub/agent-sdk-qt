9c083a031fe201c7726a3e7ec4117e8cad9e27bc
Luis Gustavo S. Barreto <gustavo@ossystems.com.br>
2018-04-12 11:20:38 -0300

    Add State::done method
    The done() method should be called after stateChanged is processed
    
    Signed-off-by: Luis Gustavo S. Barreto <gustavo@ossystems.com.br>

--- src/state.cpp
+++ src/state.cpp
@@ -17,18 +17,26 @@ State::State(): QObject()
 State::State(State::ID id, QLocalSocket *socket, QObject *parent):
     QObject(parent),
     m_id(id),
-    m_socket(socket)
+    m_socket(socket),
+    m_done(false)
 {
+}
+
+void State::done()
+{
+    m_socket->close();
+    m_done = true;
+    emit doneChanged(m_done);
 }
 
 void State::cancel()
 {
     m_socket->write("cancel");
-    m_socket->close();
+    done();
 }
 
 void State::tryAgain(int n)
 {
     m_socket->write(QString("try_again %1").arg(n).toLocal8Bit());
-    m_socket->close();
+    done();
 }
